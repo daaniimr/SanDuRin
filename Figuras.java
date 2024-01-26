6af49fa43dabff26b6fbb0fa5c744b88063ae77b
DanielDG1234 <daniel.duque5@educa,madrid.org>
2024-01-26 17:05:57 +0100

    Metodo calcular area del rectangulo

--- Figuras.java
+++ Figuras.java
@@ -14,11 +14,24 @@ public class Figuras {
         System.out.println("Área del círculo: " + areaCirculo);
 
 
+// Calcular área de un rectángulo
+        System.out.print("Ingrese la longitud del rectángulo: ");
+        double longitudRectangulo = scanner.nextDouble();
+        System.out.print("Ingrese la anchura del rectángulo: ");
+        double anchuraRectangulo = scanner.nextDouble();
+        double areaRectangulo = calcularAreaRectangulo(longitudRectangulo, anchuraRectangulo);
+        System.out.println("Área del rectángulo: " + areaRectangulo);
+
+
+
          // Método para calcular el área de un círculo
     	private static double calcularAreaCirculo(double radio) {
         	return Math.PI * Math.pow(radio, 2);
    	 }
 
-
+	// Método para calcular el área de un rectángulo
+    	private static double calcularAreaRectangulo(double longitud, double anchura) {
+        	return longitud * anchura;
+    	}
    }
 }
